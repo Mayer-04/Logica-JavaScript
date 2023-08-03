2cd2398211e9f077231c00a056fd646fbd9b356a
Mayer <andres.20.chaves@gmail.com>
2023-08-03 16:32:02 -0500

    Agregando ejercicios con arreglos

---
+++ JavaScript/ejercicio-3-midu.js
@@ -0,0 +1,19 @@
+/* Crear una función que reciba un array de palabras y devuelva true si todas las palabras terminan con la letra "a" y false si al menos una palabra no termina con la letra "a".
+
+
+Usa el método endsWith() de string para resolverlo. */
+
+function acabanEnA(words) {
+  for (let i = 0; i < words.length; i++) {
+    const terminaA = words[i].endsWith("a");
+    if (!terminaA) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Ejemplo de uso ✅
+
+const palabras = ["Hola", "Programa", "Celular", "Hoja", "Puerta"];
+const resultado = acabanEnA(palabras);
