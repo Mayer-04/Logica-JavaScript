9f040359463d67cdecc8fc00dbf959b29b60497d
Mayer <andres.20.chaves@gmail.com>
2023-08-06 17:38:59 -0500

    Agregando cambios en el código

--- JavaScript/triangulo-rectangulo.js
+++ JavaScript/triangulo-rectangulo.js
@@ -4,11 +4,11 @@ El usuario debe ingresar la longitud de los dos catetos y el programa debe mostr
 */
 
 // Validaciones pendientes
-function trianguloRectangulo(b, a) {
+const trianguloRectangulo = function (b, a) {
   const areaTriangulo = (b * a) / 2;
   const mensaje = `El área del triángulo rectángulo es: ${areaTriangulo}`;
   return mensaje;
-}
+};
 
 trianguloRectangulo(5, 6); // La área del triángulo rectángulo es: 15
 
