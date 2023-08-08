f3bf0e4c9be96d376b75be6f17c85f4bf5ae3613
Mayer <andres.20.chaves@gmail.com>
2023-08-08 16:54:19 -0500

    Ejercicio número factorial

---
+++ JavaScript/factorial.js
@@ -0,0 +1,19 @@
+/* Calcula el factorial de un número dado utilizando un bucle o una función recursiva. */
+
+const calcularFactorial = function (numero) {
+  let resultado = 1;
+  for (let i = 1; i <= numero; i++) {
+    resultado *= i;
+  }
+  return resultado;
+};
+
+// Utilizando Recursividad ✅
+
+function numeroFactorial(numero) {
+  if (numero === 0) {
+    return 1;
+  } else {
+    return numero * numeroFactorial(numero - 1);
+  }
+}
