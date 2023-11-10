bd0c58a5a47e3a0cd3fe53cd0076f8d504ae768a
Mayer Andres <andres.20.chaves@gmail.com>
2023-11-09 23:36:41 -0500

    Ejercicios con arrays

---
+++ JavaScript/ejercicios-arrays/ejercicio-6.js
@@ -0,0 +1,30 @@
+/* Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente. 
+miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }. */
+
+// Utilizando el nuevo método de JavaScript 😁
+
+function dosArreglos(arr) {
+  const ascendente = arr.toSorted();
+  const descendiente = arr.toSorted((a, b) => b - a);
+
+  return {
+    asc: ascendente,
+    desc: descendiente,
+  };
+}
+
+console.log(dosArreglos([7, 5, 7, 8, 6]));
+
+// Segunda manera de realizar el ejercicio ✅
+
+const dosArreglos = (arr) => {
+  const copia = [...arr];
+  const objeto = {};
+  const ascendente = arr.sort();
+  const descendiente = copia.sort((a, b) => b - a);
+
+  objeto.asc = ascendente;
+  objeto.desc = descendiente;
+
+  return objeto;
+};
