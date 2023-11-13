39e686d5db4e89d84256e1545b723b52b101aef0
Mayer Andres <andres.20.chaves@gmail.com>
2023-11-13 00:03:02 -0500

    Creando ejercicios con cadenas de texto

---
+++ JavaScript/cadena-texto/contar-caracteres.js
@@ -0,0 +1,16 @@
+/* Escribe una función que tome una cadena de texto y un carácter específico, y devuelva el número de veces que ese carácter aparece en la cadena. */
+
+const contarCaracteres = function (texto, caracter) {
+  let numeroCaracteres = 0;
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] === caracter) {
+      numeroCaracteres++;
+    }
+  }
+  return numeroCaracteres;
+};
+
+// Ejemplo de uso 😁
+
+const palabra = "paisaje";
+console.log(contarCaracteres(palabra, "a"));
