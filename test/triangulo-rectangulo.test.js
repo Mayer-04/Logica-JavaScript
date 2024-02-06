8b9a2001a047064d789443ba4d3603af73d4357d
Mayer Andres <andres.20.chaves@gmail.com>
2024-02-06 12:53:15 -0500

    Desinstalando jest

--- test/triangulo-rectangulo.test.js
+++
@@ -1,7 +0,0 @@
-const trianguloRectangulo = require("../JavaScript/triangulo-rectangulo.js");
-
-test("Calcular el área de un triángulo rectángulo", () => {
-  expect(trianguloRectangulo(5, 6)).toBe(
-    "El área del triángulo rectángulo es: 15"
-  );
-});
