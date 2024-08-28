81f4cd4d8bd6aa9758e910318820ea58c2c4cdc3
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-28 14:35:46 -0500

    cambios en el test del ejercicio

--- test/palindrome.test.js
+++ test/palindrome.test.js
@@ -1,9 +1,9 @@
-import { palabraPalindrome } from "../javascript/palindrome.js";
+import { isPalindrome } from "../javascript/palindrome.js";
 import { describe, test, expect } from "vitest";
 
 describe("Determinar si una palabra o frase es un palíndromo", () => {
   test("No se pasa una palabra o frase a la función", () => {
-    const resultado = palabraPalindrome();
+    const resultado = isPalindrome();
     expect(resultado).toBeNull();
   });
 
@@ -12,8 +12,8 @@ describe("Determinar si una palabra o frase es un palíndromo", () => {
     const palabra2 = "ana";
     const palabra3 = "lenguaje";
 
-    expect(palabraPalindrome(palabra)).toBe(true);
-    expect(palabraPalindrome(palabra2)).toBe(true);
-    expect(palabraPalindrome(palabra3)).toBe(false);
+    expect(isPalindrome(palabra)).toBe(true);
+    expect(isPalindrome(palabra2)).toBe(true);
+    expect(isPalindrome(palabra3)).toBe(false);
   });
 });
