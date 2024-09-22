21bc40519f23779315a41dc67e218513980f0c7c
Mayer-04 <andres.20.chaves@gmail.com>
2024-09-22 11:43:37 -0500

    add: nuevos ejercicios

--- JavaScript/callbacks-promesas/filtrar-numeros.js
+++ JavaScript/callbacks-promesas/filtrar-numeros.js
@@ -3,13 +3,13 @@ Crea una función llamada filtrarPares que tome una matriz de números y un call
 La función debería filtrar los números pares de la matriz y luego llamar al callback con el nuevo arreglo.
 */
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const filtrarPares = function (arrNumeros, callback) {
-  const numerosPares = arrNumeros.filter((par) => par % 2 === 0);
+const filtrarPares = function (numeros, callback) {
+  const numerosPares = numeros.filter((par) => par % 2 === 0);
   callback(numerosPares);
 };
 
-filtrarPares(numeros, (pares) => {
+const listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+filtrarPares(listaNumeros, (pares) => {
   console.log(pares);
 });
